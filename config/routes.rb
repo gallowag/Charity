c6e2106aff26aa0f75de8ae0733d5b4e624f743f
Greer <gallowaygreer@gmail.com>
2020-05-26 21:07:14 -0600

    Added model, controller, logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/opportunities'
+  get 'about' => 'pages#about', as: 'about'
+  get 'opportunities' => 'pages#opportunities', as: 'opportunities'
 
-  root 'pages#index'
+  root 'pages#index', as: 'home'
+
+  get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
